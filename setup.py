c8b327891c9ada265faa8e318f3216fbd32d4c12
Jonathan Sokolowski <jonathan.sokolowski@gmail.com>
2013-03-15 22:18:31 +1100

    Create PyPi distribute skeleton.

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='TrackingStateMachine',
+    version='0.1.0',
+    author='Jonathan Sokolowski',
+    author_email='jonathan.sokolowski@gmail.com',
+    packages=['tracking_state_machine', 'tracking_state_machine.test'],
+    url='http://pypi.python.org/pypi/TrackingStateMachine/',
+    license='LICENSE.txt',
+    description='State machine which can simultaneously track items in all states and manage transitions.',
+    long_description=open('README.txt').read(),
+    install_requires=[],
+)
